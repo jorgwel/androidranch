c1b60d98229f3903c73412fced3d4c06859c4318
Jorge Bautista <jorgwel@gmail.com>
2014-02-23 00:04:18 -0600

    La aplicación ya califica preguntas

--- src/com/example/geoquiz/QuizActivity.java
+++ src/com/example/geoquiz/QuizActivity.java
@@ -1,52 +1,92 @@
 package com.example.geoquiz;
 
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends Activity {
 	private Button mTrueButton;
 	private Button mFalseButton;
-	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_quiz);
-        mTrueButton = (Button)findViewById(R.id.true_button);
-        mTrueButton.setOnClickListener(new View.OnClickListener() {
-			
+	private Button mNextButton;
+	private TextView mQuestionTextView;
+
+	private TrueFalse[] mQuestionBank = new TrueFalse[] {
+			new TrueFalse(R.string.question_oceans, true),
+			new TrueFalse(R.string.question_mideast, false),
+			new TrueFalse(R.string.question_africa, false),
+			new TrueFalse(R.string.question_americas, true),
+			new TrueFalse(R.string.question_asia, true), };
+
+	private int mCurrentIndex = 0;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_quiz);
+		mTrueButton = (Button) findViewById(R.id.true_button);
+		mTrueButton.setOnClickListener(new View.OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
 				System.out.println("Botón TRUE clickeado...");
-				Toast.makeText(QuizActivity.this,
-                        R.string.correct_toast,
-                        Toast.LENGTH_SHORT).show();
-				
+				checkAnswer(true);
 			}
 		});
-        mFalseButton = (Button)findViewById(R.id.false_button);
-        mFalseButton.setOnClickListener(new View.OnClickListener() {
-			
+		mFalseButton = (Button) findViewById(R.id.false_button);
+		mFalseButton.setOnClickListener(new View.OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
 				System.out.println("Botón FALSE clickeado...");
-				Toast.makeText(QuizActivity.this,
-                        R.string.incorrect_toast,
-                        Toast.LENGTH_SHORT).show();
+				checkAnswer(false);
 			}
 		});
+		
+		mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
+		updateQuestion();
+        
+        
+        mNextButton = (Button)findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+        
+		
+	}
+	
+	private void updateQuestion(){
+		int question = mQuestionBank[mCurrentIndex].getQuestion();
+        mQuestionTextView.setText(question);
+	}
+	
+	private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+
+        int messageResId = 0;
+
+        if (userPressedTrue == answerIsTrue) {
+            messageResId = R.string.correct_toast;
+        } else {
+            messageResId = R.string.incorrect_toast;
+        }
+
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
+            .show();
     }
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.quiz, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.quiz, menu);
-        return true;
-    }
-    
 }
