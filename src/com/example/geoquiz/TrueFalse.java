8f8547cbbe225fd9b3a7947294b04e04358beef5
Jorge Bautista <jorgwel@gmail.com>
2014-02-23 21:58:40 -0600

    Agregada la clase TrueFalse

---
+++ src/com/example/geoquiz/TrueFalse.java
@@ -0,0 +1,29 @@
+package com.example.geoquiz;
+
+public class TrueFalse {
+	
+	private int mQuestion;
+	private boolean mTrueQuestion;
+	public TrueFalse(int mQuestion, boolean mTrueQuestion) {
+		this.mQuestion = mQuestion;
+		this.mTrueQuestion = mTrueQuestion;
+	}
+	
+	
+	
+	public int getQuestion() {
+		return mQuestion;
+	}
+	public void setQuestion(int question) {
+		mQuestion = question;
+	}
+	public boolean isTrueQuestion() {
+		return mTrueQuestion;
+	}
+	public void setTrueQuestion(boolean trueQuestion) {
+		mTrueQuestion = trueQuestion;
+	}
+
+	
+
+}
