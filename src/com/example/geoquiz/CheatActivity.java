43282f9f0890bfb0c1a6515db3daad10a8097d06
Jorge Bautista <jorgwel@gmail.com>
2014-03-05 07:17:44 -0600

    Users can rotate CheatActivity after they cheat to clear out the cheating result.

--- src/com/example/geoquiz/CheatActivity.java
+++ src/com/example/geoquiz/CheatActivity.java
@@ -9,6 +9,7 @@ package com.example.geoquiz;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -18,12 +19,16 @@ import android.widget.TextView;
  * @author jorgwel
  */
 public class CheatActivity extends Activity {
-
+    private static final String TAG = "CheatActivity";
+    private static final String KEY_HAS_CHEATED = "hasCheated";
+    private static final String KEY_ANSWER_TRUE = "answerIsTrue";
+    
     public static final String EXTRA_ANSWER_IS_TRUE = "com.bignerdranch.android.geoquiz.answer_is_true";
     public static final String EXTRA_ANSWER_SHOWN = "com.bignerdranch.android.geoquiz.answer_shown";
 
     
     private boolean mAnswerIsTrue;
+    private boolean hasCheated;
 
     private TextView mAnswerTextView;
     private Button mShowAnswer;
@@ -47,14 +52,20 @@ public class CheatActivity extends Activity {
         mShowAnswer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (mAnswerIsTrue) {
-                    mAnswerTextView.setText(R.string.true_button);
-                } else {
-                    mAnswerTextView.setText(R.string.false_button);
-                }
-                setAnswerShownResult(true);
+                mostrarResultado();
             }
+
         });
+        
+        if(savedInstanceState != null) {
+            Log.d(TAG, "Recuperando info de hasCheated");
+            hasCheated = savedInstanceState.getBoolean(KEY_HAS_CHEATED);
+            mAnswerIsTrue = savedInstanceState.getBoolean(KEY_ANSWER_TRUE);
+            if(hasCheated){
+                mostrarResultado();
+            }
+        }
+        
         // ToDo add your GUI initialization code here        
     }
     
@@ -62,6 +73,28 @@ public class CheatActivity extends Activity {
         Intent data = new Intent();
         data.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown);
         setResult(RESULT_OK, data);
+        hasCheated = isAnswerShown;
+    }
+    
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        setAnswerShownResult(false);
+        savedInstanceState.putBoolean(KEY_HAS_CHEATED, hasCheated);
+        savedInstanceState.putBoolean(KEY_ANSWER_TRUE, mAnswerIsTrue);
+        
+    }
+
+    
+    private void mostrarResultado() {
+        if (mAnswerIsTrue) {
+            mAnswerTextView.setText(R.string.true_button);
+        } else {
+            mAnswerTextView.setText(R.string.false_button);
+        }
+        setAnswerShownResult(true);
+        
     }
     
 }
