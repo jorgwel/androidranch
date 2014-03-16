97b210128ab020e34a592eee57f6391e93250d40
Jorge Bautista <jorgwel@localhost.localdomain>
2014-03-15 19:31:14 -0600

    Aplicación terminada hasta 'Mostrar un cuadro de diálogo en el fragmento'

---
+++ CriminalIntent/src/bignerdranch/android/criminalintent/Crime.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bignerdranch.android.criminalintent;
+
+import java.util.UUID;
+
+/**
+ *
+ * @author jorgwel
+ */
+public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+
+    public Crime() {
+        // Generate unique identifier
+        mId = UUID.randomUUID();
+    }
+    
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+}
--- CriminalIntent/src/bignerdranch/android/criminalintent/CrimeActivity.java
+++ CriminalIntent/src/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -1,15 +1,27 @@
 package bignerdranch.android.criminalintent;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 
-public class CrimeActivity extends Activity
+public class CrimeActivity extends FragmentActivity
 {
-    /** Called when the activity is first created. */
+    /** Called when the activity is first created. */    
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.activity_crime);
+        
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            fm.beginTransaction()
+                .add(R.id.fragmentContainer, fragment)
+                .commit();
+        }
+        
     }
 }
