039414d21821e9de3606ff8b043a664578c79302
Jorge Bautista <jorgwel@localhost.localdomain>
2014-03-17 10:04:54 -0600

    Se muestra un formato personalizado en la fecha

--- CriminalIntent/src/bignerdranch/android/criminalintent/CrimeFragment.java
+++ CriminalIntent/src/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,8 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
+import java.text.Format;
+import java.text.SimpleDateFormat;
 
 public class CrimeFragment extends Fragment{
     private Crime mCrime;
@@ -47,8 +50,8 @@ public class CrimeFragment extends Fragment{
             }
         });
         
-        mDateButton = (Button)v.findViewById(R.id.crime_date);
-        mDateButton.setText(mCrime.getDate().toString());
+        mDateButton = (Button)v.findViewById(R.id.crime_date);        
+        mDateButton.setText(DateFormat.format("EEEE, dd MMMM, yyyy", mCrime.getDate()));
         mDateButton.setEnabled(false);
         
         mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
