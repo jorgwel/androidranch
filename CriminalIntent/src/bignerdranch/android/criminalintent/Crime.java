f2993605aca6207707c269c50b3c27f4383c80fc
Jorge Bautista <jorgwel@localhost.localdomain>
2014-03-17 09:38:24 -0600

    Se ha creado un layout horizontal

--- CriminalIntent/src/bignerdranch/android/criminalintent/Crime.java
+++ CriminalIntent/src/bignerdranch/android/criminalintent/Crime.java
@@ -5,6 +5,7 @@
  */
 package bignerdranch.android.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -15,10 +16,13 @@ public class Crime {
 
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
 
     public Crime() {
         // Generate unique identifier
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
     
     public UUID getId() {
@@ -32,4 +36,19 @@ public class Crime {
     public void setTitle(String title) {
         mTitle = title;
     }
+    
+    public Date getDate() {
+        return mDate;
+    }
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+    
 }
