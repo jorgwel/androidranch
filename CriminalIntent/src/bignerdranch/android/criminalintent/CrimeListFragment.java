41ed7cbf5834dd60210d8431f97fa1d5c56d6de2
Jorge Bautista <jorgwel@localhost.localdomain>
2014-03-24 00:29:31 -0600

    Ya se escuchan los clicks/toques del usuario.

--- CriminalIntent/src/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ CriminalIntent/src/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -2,11 +2,17 @@ package bignerdranch.android.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import java.util.ArrayList;
 
 public class CrimeListFragment extends ListFragment {
     private ArrayList<Crime> mCrimes;
+    
+    private static final String TAG = "CrimeListFragment";
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,12 @@ public class CrimeListFragment extends ListFragment {
                                    android.R.layout.simple_list_item_1,
                                    mCrimes);
         setListAdapter(adapter);
+    }
+    
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        Crime c = (Crime)(getListAdapter()).getItem(position);
+        Log.d(TAG, c.getTitle() + " was clicked");
     }
     
 }
