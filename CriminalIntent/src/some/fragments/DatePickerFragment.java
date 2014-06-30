7a21da07825623cadccce997cd3042ea6f941ce4
Jorge Bautista <jorgwel@gmail.com>
2014-06-29 21:43:42 -0500

    Se ha agregado un diálogo que muestra el date picker en todo su esplendor

--- CriminalIntent/src/some/fragments/DatePickerFragment.java
+++ CriminalIntent/src/some/fragments/DatePickerFragment.java
@@ -10,6 +10,7 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.view.View;
 import bignerdranch.android.criminalintent.R;
 
 /**
@@ -21,7 +22,12 @@ public class DatePickerFragment extends DialogFragment {
     
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        
+        View calendarView = getActivity().getLayoutInflater()
+        .inflate(R.layout.dialog_date, null);
+        
         return new AlertDialog.Builder(getActivity())
+            .setView(calendarView)
             .setTitle(R.string.date_picker_title)
             .setPositiveButton(android.R.string.ok, null)
             .create();
