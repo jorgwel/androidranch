149adeb1db7fb3a0794e9ee2bfb74043e4fa4a0a
Jorge Bautista <jorgwel@gmail.com>
2014-06-29 23:26:26 -0500

    Se actualiza la fecha en el DatePicker y se mantiene durante rotaciones.

--- CriminalIntent/src/bignerdranch/android/criminalintent/CrimeFragment.java
+++ CriminalIntent/src/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -69,7 +69,8 @@ public class CrimeFragment extends Fragment{
             public void onClick(View v) {
                 FragmentManager fm = getActivity()
                         .getSupportFragmentManager();
-                DatePickerFragment dialog = new DatePickerFragment();
+//                DatePickerFragment dialog = new DatePickerFragment();
+                DatePickerFragment dialog = DatePickerFragment.newInstance(mCrime.getDate());
                 dialog.show(fm, DIALOG_DATE);
             }
         });
--- CriminalIntent/src/some/fragments/DatePickerFragment.java
+++ CriminalIntent/src/some/fragments/DatePickerFragment.java
@@ -10,8 +10,14 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.util.Log;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.DatePicker.OnDateChangedListener;
 import bignerdranch.android.criminalintent.R;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 /**
  *
@@ -19,12 +25,52 @@ import bignerdranch.android.criminalintent.R;
  */
 public class DatePickerFragment extends DialogFragment {
     
+    private static final String TAG = "DatePickerFragment";
+    
+    public static final String EXTRA_DATE =
+        "com.bignerdranch.android.criminalintent.date";
+
+    private Date mDate;
+
+    public static DatePickerFragment newInstance(Date date) {
+        Bundle args = new Bundle();
+        args.putSerializable(EXTRA_DATE, date);
+
+        DatePickerFragment fragment = new DatePickerFragment();
+        fragment.setArguments(args);
+
+        return fragment;
+    }
     
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         
+        mDate = (Date)getArguments().getSerializable(EXTRA_DATE);
+
+        // Create a Calendar to get the year, month, and day
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(mDate);
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+    
+        
+        
         View calendarView = getActivity().getLayoutInflater()
         .inflate(R.layout.dialog_date, null);
+        
+        DatePicker datePicker = (DatePicker)calendarView.findViewById(R.id.dialog_date_datePicker);
+        datePicker.init(year, month, day, new OnDateChangedListener() {
+            public void onDateChanged(DatePicker view, int year, int month, int day) {
+                // Translate year, month, day into a Date object using a calendar
+                mDate = new GregorianCalendar(year, month, day).getTime();
+
+                // Update argument to preserve selected value on rotation
+                getArguments().putSerializable(EXTRA_DATE, mDate);
+                Log.i(TAG, "Seteando nueva fecha: " + mDate);
+            }
+        });
         
         return new AlertDialog.Builder(getActivity())
             .setView(calendarView)
